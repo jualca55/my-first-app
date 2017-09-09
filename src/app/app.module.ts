4d93b0953a6dd36c3564c8a92675cf80324ee3ea
jualca55 <jualca55@gmail.com>
2017-09-09 15:00:13 -0500

    se crean componentes, menú, current-option e index

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { CurrentOptionComponent } from './current-option/current-option.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    CurrentOptionComponent
   ],
   imports: [
     BrowserModule
