7b2a7d87527552343f11b5cc698e8be9400462c2
jualca55 <jualca55@gmail.com>
2017-09-10 11:44:25 -0500

    se agregan los componentes list y add en el modulo customers

---
+++ src/app/customers/add/add.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import {Customer} from '../customer';
+
+@Component({
+  selector: 'customers-add',
+  templateUrl: './add.component.html',
+  styleUrls: ['./add.component.css']
+})
+export class AddComponent implements OnInit {
+
+  customer:Customer;
+
+  constructor() {
+       }
+
+  ngOnInit() {
+    this.customer = new Customer();
+    }
+
+}
---
+++ src/app/customers/customer.ts
@@ -0,0 +1,7 @@
+export class Customer{
+    name: string;
+    address: string;
+    stratum: number;
+    gender: string   
+   
+}
